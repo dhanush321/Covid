a08d1ed537087a619f2f8d66f65b76a35d5083ea
Dhanush Krishnan <krishna.awesome@gmail.com>
2020-05-24 19:07:59 -0600

    new

---
+++ app.js
@@ -0,0 +1,520 @@
+const afghanistan = document.querySelector('#af')
+const albania = document.querySelector('#al')
+const algeria = document.querySelector('#dz')
+const andorra = document.querySelector('#ad')
+const angola = document.querySelector('#ao')
+const antigua = document.querySelector('#ag')
+const argentina = document.querySelector('#ar')
+const armenia = document.querySelector('#am')
+const australia = document.querySelector('#au')
+const austria = document.querySelector('#at')
+const azerbaijan = document.querySelector('#az')
+const bahamas = document.querySelector('#bs')
+const bahrain = document.querySelector('#bh')
+const bangladesh = document.querySelector('#bd')
+const barbados = document.querySelector('#bb')
+const belarus = document.querySelector('#by')
+const belgium = document.querySelector('#be')
+const belize = document.querySelector('#bz')
+const benin = document.querySelector('#bj')
+const bhutan = document.querySelector('#bt')
+const bolivia = document.querySelector('#bo')
+const bosnia = document.querySelector('#ba')
+const botswana = document.querySelector('#bw')
+const brazil = document.querySelector('#br')
+const brunei = document.querySelector('#bn')
+const bulgaria = document.querySelector('#bg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+// const india = document.querySelector('#in')
+// const canada = document.querySelector('#ca')
+// const australia = document.querySelector('#au')
+// const singapore = document.querySelector('#sg')
+// const america = document.querySelector('#us')
+// const america = document.querySelector('#us')
+
+
+function global(data) {
+
+    const details = document.createElement("div")
+    details.className= "details"
+
+    const gcases = document.createElement("h1")
+    gcases.className = "global"
+    gcases.textContent = "Global Cases"
+
+    const newConfirmed = document.createElement("p")
+    newConfirmed.textContent = "New Confirmed: " + data.Global.NewConfirmed
+
+    const totalConfirmed = document.createElement("p")
+    totalConfirmed.textContent = "Total Confirmed: " + data.Global.TotalConfirmed
+
+    const newDeaths = document.createElement("p")
+    newDeaths.textContent = "New Deaths: " + data.Global.NewDeaths
+
+    const totalDeaths = document.createElement("p")
+    totalDeaths.textContent = "Total Deaths: " + data.Global.TotalDeaths
+
+    const newRecovered = document.createElement("p")
+    newRecovered.textContent = "New Recovered: " +data.Global.NewRecovered
+
+    const totalRecovered = document.createElement("p")
+    totalRecovered.textContent = "Total Recovered: " +data.Global.TotalRecovered
+
+    const lastUpdate = document.createElement("p")
+    lastUpdate.className = "update"
+    lastUpdate.textContent = "Last Updated: " +data.Date
+
+
+    details.appendChild(gcases)
+    details.appendChild(newConfirmed)
+    details.appendChild(totalConfirmed)
+    details.appendChild(newDeaths)
+    details.appendChild(totalDeaths)
+    details.appendChild(newRecovered)
+    details.appendChild(totalRecovered)
+    details.appendChild(lastUpdate)
+    document.querySelector('section').appendChild(details)
+}
+
+function covid(data) {
+
+    const details = document.createElement("div")
+    details.className= "details"
+
+    const country = document.createElement("h1")
+    country.textContent =  data.Countries[x].Country
+
+    const newConfirmed = document.createElement("p")
+    newConfirmed.textContent = "New Confirmed: " + data.Countries[x].NewConfirmed
+
+    const totalConfirmed = document.createElement("p")
+    totalConfirmed.textContent = "Total Confirmed: " + data.Countries[x].TotalConfirmed
+
+    const newDeaths = document.createElement("p")
+    newDeaths.textContent = "New Deaths: " + data.Countries[x].NewDeaths
+
+    const totalDeaths = document.createElement("p")
+    totalDeaths.textContent = "Total Deaths: " + data.Countries[x].TotalDeaths
+
+    const newRecovered = document.createElement("p")
+    newRecovered.textContent = "New Recovered: " +data.Countries[x].NewRecovered
+
+    const totalRecovered = document.createElement("p")
+    totalRecovered.textContent = "Total Recovered: " +data.Countries[x].TotalRecovered
+
+    const lastUpdate = document.createElement("p")
+    lastUpdate.className = "update"
+    lastUpdate.textContent = "Last Updated: " +data.Countries[x].Date
+
+
+    details.appendChild(country)
+    details.appendChild(newConfirmed)
+    details.appendChild(totalConfirmed)
+    details.appendChild(newDeaths)
+    details.appendChild(totalDeaths)
+    details.appendChild(newRecovered)
+    details.appendChild(totalRecovered)
+    details.appendChild(lastUpdate)
+    document.querySelector('section').appendChild(details)
+}
+
+const url = "https://api.covid19api.com/summary"
+
+fetch(url)
+    .then(x => x.json())
+    .then(data => {
+        global(data)
+        function clear(){
+            document.querySelector('section').innerHTML=''
+        }
+        afghanistan.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AF"){
+               covid(data)
+           }
+        }
+        })
+        albania.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AL"){
+               covid(data)
+           }
+        }
+        })
+        algeria.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "DZ"){
+               covid(data)
+           }
+        }
+        })
+        // andorra.addEventListener('click', function() {
+        //     clear()
+        // for(x=0; x<=185; x++){
+        //    if (data.Countries[x].CountryCode === "AD"){
+        //        covid(data)
+        //    }
+        // }
+        // })
+        angola.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AO"){
+               covid(data)
+           }
+        }
+        })
+        // antigua.addEventListener('click', function() {
+        //     clear()
+        // for(x=0; x<=185; x++){
+        //    if (data.Countries[x].CountryCode === "AG"){
+        //        covid(data)
+        //    }
+        // }
+        // })
+        argentina.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AR"){
+               covid(data)
+           }
+        }
+        })
+        armenia.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AM"){
+               covid(data)
+           }
+        }
+        })
+        australia.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AU"){
+               covid(data)
+           }
+        }
+        })
+        austria.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AT"){
+               covid(data)
+           }
+        }
+        
+        })
+        azerbaijan.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "AZ"){
+               covid(data)
+           }
+        }
+        
+        })
+        bahamas.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BS"){
+               covid(data)
+           }
+        }
+        
+        })
+        // bahrain.addEventListener('click', function() {
+        //     clear()
+        // for(x=0; x<=185; x++){
+        //    if (data.Countries[x].CountryCode === "BH"){
+        //        covid(data)
+        //    }
+        // }
+        
+        // })
+        bangladesh.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BD"){
+               covid(data)
+           }
+        }
+        })
+        // barbados.addEventListener('click', function() {
+        //     clear()
+        // for(x=0; x<=185; x++){
+        //    if (data.Countries[x].CountryCode === "BB"){
+        //        covid(data)
+        //    }
+        // }
+        // })
+        belarus.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BY"){
+               covid(data)
+           }
+        }
+        })
+        belgium.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BE"){
+               covid(data)
+           }
+        }
+        })
+        belize.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BZ"){
+               covid(data)
+           }
+        }
+        })
+        benin.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BJ"){
+               covid(data)
+           }
+        }
+        })
+        bhutan.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BT"){
+               covid(data)
+           }
+        }
+        })
+        bolivia.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BO"){
+               covid(data)
+           }
+        }
+        })
+        bosnia.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BA"){
+               covid(data)
+           }
+        }
+        })
+        botswana.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BW"){
+               covid(data)
+           }
+        }
+        
+        })
+        brazil.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BR"){
+               covid(data)
+           }
+        }
+        
+        })
+        brunei.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BN"){
+               covid(data)
+           }
+        }
+        
+        })
+        bulgaria.addEventListener('click', function() {
+            clear()
+        for(x=0; x<=185; x++){
+           if (data.Countries[x].CountryCode === "BG"){
+               covid(data)
+           }
+        }
+        
+        })
+    })
+
+    // var f =function() {
+    //     function eventHandler(event){
+    //         var width = parseInt(window.getComputedStyle(this).width);
+    //         var height= parseInt(window.getComputedStyle(this).height);
+    //         var zoom=40;
+
+    //         if(event.wheelDelta > 0){
+    //             this.style.width = Math.min(1000, width + zoom) + "px";
+    //             this.style.height = Math.min(1000, height + zoom) + "px";
+    //         }
+    //         else{
+    //             this.style.width = Math.max(800, width - zoom) + "px";
+    //             this.style.height = Math.max(800, height - zoom) + "px";
+    //         }
+    //         event.preventDefault();
+    //     }
+    //     var imageElement = document.getElementsByTagName('svg')[0];
+    //     imageElement.addEventListener("wheel",eventHandler,false);
+    // }
+    // window.addEventListener('load',f,false);
